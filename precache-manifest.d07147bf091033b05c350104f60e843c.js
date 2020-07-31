a0066423c72f5713eeace14e04696bdb217e4ea8
Elijah <delosreyes.elijah@gmail.com>
2020-07-30 20:52:59 -0700

    Updates

---
+++ precache-manifest.d07147bf091033b05c350104f60e843c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d5589b6299d74419fe8e8e0cc859ed",
+    "url": "/twitter-react/index.html"
+  },
+  {
+    "revision": "249c8b5370366ca39bde",
+    "url": "/twitter-react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "969c55d68140177b59f6",
+    "url": "/twitter-react/static/css/main.f742470d.chunk.css"
+  },
+  {
+    "revision": "249c8b5370366ca39bde",
+    "url": "/twitter-react/static/js/2.e05b5d67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/twitter-react/static/js/2.e05b5d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969c55d68140177b59f6",
+    "url": "/twitter-react/static/js/main.fa34252e.chunk.js"
+  },
+  {
+    "revision": "c9a3cab0d25e95dd3433",
+    "url": "/twitter-react/static/js/runtime-main.ab9f5a60.js"
+  }
+]);
