61e4326cd88aa264e570dcb5da3332653ffb5c4e
Elijah <delosreyes.elijah@gmail.com>
2020-08-08 19:56:54 -0700

    Updates

---
+++ precache-manifest.01c57d23a0f76033f6ebf9b26ff37a7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac44e0fd3924cad1f8b990a91144505",
+    "url": "/twitter-react/index.html"
+  },
+  {
+    "revision": "e12020c85fe6d3c39ec2",
+    "url": "/twitter-react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "97549fd2c19aecaee0aa",
+    "url": "/twitter-react/static/css/main.adbf1ff6.chunk.css"
+  },
+  {
+    "revision": "e12020c85fe6d3c39ec2",
+    "url": "/twitter-react/static/js/2.70677346.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/twitter-react/static/js/2.70677346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97549fd2c19aecaee0aa",
+    "url": "/twitter-react/static/js/main.cacaa9ac.chunk.js"
+  },
+  {
+    "revision": "8804427ee7ae0b7412c6",
+    "url": "/twitter-react/static/js/runtime-main.65fa7dcf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/twitter-react/precache-manifest.840f4c985a5ab2dd054e7e80fed9fc06.js"
+  "/twitter-react/precache-manifest.01c57d23a0f76033f6ebf9b26ff37a7f.js"
 );
 
 self.addEventListener('message', (event) => {
